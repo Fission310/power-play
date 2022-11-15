e0e92ef69e2fcb4ac584f081e6cd1db0a6e4cf32
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-11-15 13:54:35 -0500

    robot dev

--- hardware/Robot.java
+++ hardware/Robot.java
@@ -1,2 +1,40 @@
-package org.firstinspires.ftc.teamcode.hardware;public class Robot {
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.stuyfission.fissionlib.util.Mechanism;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Robot extends Mechanism {
+
+    private Drivetrain dt = new Drivetrain(opMode);
+    private Intake intake = new Intake(opMode);
+    private MXSensor mxSensor = new MXSensor(opMode);
+    private SlidesFSM slidesFSM = new SlidesFSM(opMode);
+
+    public Robot(LinearOpMode opMode) { this.opMode = opMode; }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        dt.init(hwMap);
+        intake.init(hwMap);
+        mxSensor.init(hwMap);
+        slidesFSM.init(hwMap);
+    }
+
+    @Override
+    public void loop(Gamepad gamepad) {
+        dt.loop(gamepad);
+        intake.loop(gamepad);
+        mxSensor.loop(gamepad);
+        slidesFSM.loop(gamepad);
+    }
+
+    @Override
+    public void telemetry(Telemetry telemetry) {
+        mxSensor.telemetry(telemetry);
+        slidesFSM.telemetry(telemetry);
+    }
 }
--- opmode/dev/DevRobot.java
+++ opmode/dev/DevRobot.java
@@ -1,2 +1,44 @@
-package org.firstinspires.ftc.teamcode.opmode.dev;public class DevRobot {
+package org.firstinspires.ftc.teamcode.opmode.dev;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+
+import java.util.List;
+
+@TeleOp (name = "Dev Robot", group = "dev")
+public class DevRobot extends LinearOpMode {
+
+    private Robot robot = new Robot(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        // Bulk Reading
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            robot.loop(gamepad1);
+
+            // clear cache
+            for (LynxModule hub : allHubs) {
+                hub.clearBulkCache();
+            }
+            robot.telemetry(telemetry);
+            telemetry.update();
+        }
+    }
 }
