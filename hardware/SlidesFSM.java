b574abd11a57b8acf2298551da049cb2d86c432d
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-11-17 03:09:02 -0500

    threading done right

--- hardware/SlidesFSM.java
+++ hardware/SlidesFSM.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -10,13 +9,21 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class SlidesFSM extends Mechanism {
 
-    SlidesMotors slidesMotors = new SlidesMotors(opMode);
+    private SlidesMotors slidesMotors = new SlidesMotors(opMode);
+
+    private Thread lowThread;
+    private Thread mediumThread;
+    private Thread highThread;
 
     public SlidesFSM(LinearOpMode opMode) { this.opMode = opMode; }
 
     @Override
     public void init(HardwareMap hwMap) {
         slidesMotors.init(hwMap);
+
+        lowThread = new Thread(low);
+        mediumThread = new Thread(medium);
+        highThread = new Thread(high);
     }
 
     public enum SlidesState {
@@ -59,6 +66,7 @@ public class SlidesFSM extends Mechanism {
 
     @Override
     public void loop(Gamepad gamepad) {
+        slidesMotors.update();
         switch (slidesState) {
             case REST:
                 slidesMotors.rest();
@@ -66,13 +74,19 @@ public class SlidesFSM extends Mechanism {
                 break;
             case WAIT_EXTEND:
                 if (gamepad.a) {
-                    low.run();
+                    try {
+                        lowThread.start();
+                    } catch (IllegalThreadStateException ignored) {}
                 }
                 if (gamepad.b) {
-                    medium.run();
+                    try {
+                        mediumThread.start();
+                    } catch (IllegalThreadStateException ignored) {}
                 }
                 if (gamepad.y) {
-                    high.run();
+                    try {
+                        highThread.start();
+                    } catch (IllegalThreadStateException ignored) {}
                 }
                 break;
             case WAIT_RETRACT:
