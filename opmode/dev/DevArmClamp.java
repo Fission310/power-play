f8eca38fc9655c5dff127cac7d97ceb054056f0a
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-11-29 03:12:47 -0500

    test opmode

--- opmode/dev/DevArmClamp.java
+++ opmode/dev/DevArmClamp.java
@@ -1,2 +1,27 @@
-package org.firstinspires.ftc.teamcode.opmode.dev;public class DevArmClamp {
+package org.firstinspires.ftc.teamcode.opmode.dev;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Clamp;
+
+@TeleOp (name = "Dev Arm & Clamp", group = "dev")
+public class DevArmClamp extends LinearOpMode {
+
+    private Arm arm = new Arm(this);
+    private Clamp clamp = new Clamp(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        arm.init(hardwareMap);
+        clamp.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            arm.loop(gamepad1);
+            clamp.loop(gamepad1);
+        }
+    }
 }
