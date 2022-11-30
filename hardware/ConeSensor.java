a991763f8b72e18904c2ca9ecc81a4650b171cf3
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-11-30 03:14:24 -0500

    implement color sensor

---
+++ hardware/ConeSensor.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.stuyfission.fissionlib.util.Mechanism;
+
+public class ConeSensor extends Mechanism {
+
+    private ColorRangeSensor coneSensor;
+
+    public static double THRESHOLD = 100;
+
+    public ConeSensor(LinearOpMode opMode) { this.opMode = opMode; }
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        coneSensor = hwMap.get(ColorRangeSensor.class, "coneSensor");
+    }
+
+    private int getBlue(ColorRangeSensor color) {
+        return color.blue();
+    }
+
+    private int getRed(ColorRangeSensor color) {
+        return color.red();
+    }
+
+    private boolean hasConeColor(ColorRangeSensor color) {
+        int blue = getBlue(color);
+        int red = getRed(color);
+
+        return ((blue >= THRESHOLD) || (red >= THRESHOLD));
+    }
+
+    public boolean hasCone() {
+        return hasConeColor(coneSensor);
+    }
+
+}
