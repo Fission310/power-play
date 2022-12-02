bf0bbe3bf3a070ac7f73cccdd29c088e44950685
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-12-02 13:40:44 -0500

    test opmode

--- opmode/dev/TestMotorDirection.java
+++ opmode/dev/TestMotorDirection.java
@@ -1,2 +1,52 @@
-package org.firstinspires.ftc.teamcode.opmode.dev;public class TestMotorDirection {
+package org.firstinspires.ftc.teamcode.opmode.dev;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Config
+@TeleOp (name = "TestMotorDirection", group = "dev")
+public class TestMotorDirection extends LinearOpMode {
+
+//    DcMotorEx leftSlideMotor;
+    DcMotorEx rightSlideMotor;
+
+    public static double POWER = 0.4;
+
+    public void rotate() {
+//        leftSlideMotor.setPower(POWER);
+        rightSlideMotor.setPower(POWER);
+    }
+
+    public void reverse() {
+        rightSlideMotor.setPower(-POWER);
+    }
+
+    public void stopRotation() {
+//        leftSlideMotor.setPower(0);
+        rightSlideMotor.setPower(0);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+//        leftSlideMotor = hardwareMap.get(DcMotorEx.class, "leftSlideMotor");
+//        leftSlideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        rightSlideMotor = hardwareMap.get(DcMotorEx.class, "rightSlideMotor");
+        rightSlideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            if (gamepad1.right_trigger > 0) {
+                rotate();
+            } else if (gamepad1.left_trigger > 0) {
+                reverse();
+            } else {
+                stopRotation();
+            }
+        }
+    }
 }
