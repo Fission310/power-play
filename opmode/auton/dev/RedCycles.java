102bd5f26257776c01e0d6364bcb243199157ec2
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-12-12 14:22:35 -0500

    work on auto&paths

--- opmode/auton/dev/RedCycles.java
+++ opmode/auton/dev/RedCycles.java
@@ -1,2 +1,140 @@
-package org.firstinspires.ftc.teamcode.opmode.auton.dev.paths;public class RedCycles {
+package org.firstinspires.ftc.teamcode.opmode.auton.dev;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.QuinticSpline;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous (name = "Red Cycles", group = "_main")
+public class RedCycles extends LinearOpMode {
+
+    private SampleMecanumDrive drive;
+
+//    private static final double HEADING = Math.toRadians(180);
+//    private static final double WALL_POS = -1 * (70.5-(14/2.0));
+//
+//    private static final double RIGHT_CENTER_X = 35;
+//
+//    private static final double RIGHT_HIGH_GOAL_Y = 0;
+//    private static final double RIGHT_HIGH_GOAL_SCORE_X = 28;
+//
+//    private static final double RIGHT_CONE_STACK_Y = -13;
+//    private static final double RIGHT_CONE_STACK_X = 62;
+//
+//    private static final Pose2d RIGHT_START_POSE = new Pose2d(RIGHT_CENTER_X, WALL_POS, HEADING);
+
+    // FOR SPLINE CYCLE //
+    private static final double HEADING = Math.toRadians(180);
+    private static final double WALL_POS = -1 * (70.5-(14/2.0));
+
+    private static final double RIGHT_CENTER_X = 35;
+    private static final Pose2d RIGHT_START_POSE = new Pose2d(RIGHT_CENTER_X, WALL_POS, HEADING);
+
+    private static final double RIGHT_CONE_STACK_Y = -13;
+    private static final double RIGHT_CONE_STACK_X = 62;
+
+    private static final double RIGHT_HIGH_GOAL_X = 25;
+    private static final double RIGHT_HIGH_GOAL_Y = 4;
+
+    private static final Pose2d HIGH_GOAL_POSE = new Pose2d(RIGHT_HIGH_GOAL_X, RIGHT_HIGH_GOAL_Y, Math.toRadians(135));
+    private static final Vector2d HIGH_GOAL_VECTOR = new Vector2d(RIGHT_HIGH_GOAL_X, RIGHT_HIGH_GOAL_Y);
+    private static final Vector2d CONE_STACK = new Vector2d(RIGHT_CONE_STACK_X, RIGHT_CONE_STACK_Y);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setPoseEstimate(RIGHT_START_POSE);
+
+//        TrajectorySequence sequence = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
+//                // DRIVE TO HIGH GOAL WITH PRELOAD //
+//                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_HIGH_GOAL_Y, HEADING))
+//                .lineToLinearHeading(new Pose2d(RIGHT_HIGH_GOAL_SCORE_X, RIGHT_HIGH_GOAL_Y, HEADING))
+//                // DRIVE TO HIGH GOAL WITH PRELOAD //
+//
+//                // SCORE //
+//                .waitSeconds(1)
+//                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_HIGH_GOAL_Y, HEADING))
+//                // SCORE //
+//
+//                // GO TO CONE STACK //
+//                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_CONE_STACK_Y, HEADING))
+//                .lineToLinearHeading(new Pose2d(RIGHT_CONE_STACK_X, RIGHT_CONE_STACK_Y, HEADING))
+//                // GO TO CONE STACK //
+//
+//                .waitSeconds(0.5)
+//
+//                // GO BACK TO HIGH GOAL //
+//                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_CONE_STACK_Y, HEADING))
+//                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_HIGH_GOAL_Y, HEADING))
+//                .lineToLinearHeading(new Pose2d(RIGHT_HIGH_GOAL_SCORE_X, RIGHT_HIGH_GOAL_Y, HEADING))
+//                // GO BACK TO HIGH GOAL //
+//
+//
+//                .build();
+
+        TrajectorySequence splineCycle = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
+                // drive to high goal
+                .setTangent(Math.toRadians(90))
+                .splineToLinearHeading(HIGH_GOAL_POSE, Math.toRadians(125))
+                // drive to high goal
+
+                // score
+                .waitSeconds(0.5)
+                // score
+
+                // ======================= //
+
+                // start cycle
+
+                // go to cone stack
+                .setReversed(true)
+                .setTangent(Math.toRadians(315))
+                .splineTo(CONE_STACK, Math.toRadians(0))
+
+                .waitSeconds(0.5)
+
+                // go to score
+                .setReversed(false)
+                .setTangent(Math.toRadians(180))
+                .splineTo(HIGH_GOAL_VECTOR, Math.toRadians(135))
+
+                .waitSeconds(0.5)
+
+                // end cycle
+
+                // ======================= //
+
+                // start cycle
+
+                // go to cone stack
+                .setReversed(true)
+                .setTangent(Math.toRadians(315))
+                .splineTo(CONE_STACK, Math.toRadians(0))
+
+                .waitSeconds(0.5)
+
+                // go to score
+                .setReversed(false)
+                .setTangent(Math.toRadians(180))
+                .splineTo(HIGH_GOAL_VECTOR, Math.toRadians(135))
+
+                .waitSeconds(0.5)
+
+                // end cycle
+                .build();
+
+        waitForStart();
+
+        drive.followTrajectorySequenceAsync(splineCycle);
+
+        while (opModeIsActive() && !isStopRequested()) {
+            drive.update();
+        }
+    }
+
 }
--- opmode/auton/dev/paths/RedPath.java
+++ opmode/auton/dev/paths/RedPath.java
@@ -12,99 +12,31 @@ public class RedPath {
 
     private SampleMecanumDrive drive;
 
-    private static final double HEADING = Math.toRadians(270);
+    private static final double HEADING = Math.toRadians(180);
     private static final double WALL_POS = -1 * (70.5-(14/2.0));
 
-    public Pose2d LEFT_START_POSE = new Pose2d(LEFT_CENTER_X, WALL_POS, HEADING);
-
-    private static final double LEFT_CENTER_X = -35;
-    private static final double LEFT_CENTER_Y = -35;
-    private static final double LEFT_LEFT_X = -60;
-    private static final double LEFT_RIGHT_X = -12;
-
-    private static final Pose2d LEFT_PARK_LEFT = new Pose2d(LEFT_LEFT_X, LEFT_CENTER_Y, HEADING);
-    private static final Pose2d LEFT_PARK_MIDDLE = new Pose2d(LEFT_CENTER_X, LEFT_CENTER_Y, HEADING);
-    private static final Pose2d LEFT_PARK_RIGHT = new Pose2d(LEFT_RIGHT_X, LEFT_CENTER_Y, HEADING);
-
-    // =============================================== //
-
-    public Pose2d RIGHT_START_POSE = new Pose2d(RIGHT_CENTER_X, WALL_POS, HEADING);
-
     private static final double RIGHT_CENTER_X = 35;
-    private static final double RIGHT_CENTER_Y = -35;
-    private static final double RIGHT_LEFT_X = 12;
-    private static final double RIGHT_RIGHT_X = 60;
 
-    private static final Pose2d RIGHT_PARK_LEFT = new Pose2d(RIGHT_LEFT_X, RIGHT_CENTER_Y, HEADING);
-    private static final Pose2d RIGHT_PARK_MIDDLE = new Pose2d(RIGHT_CENTER_X, RIGHT_CENTER_Y, HEADING);
-    private static final Pose2d RIGHT_PARK_RIGHT = new Pose2d(RIGHT_RIGHT_X, RIGHT_CENTER_Y, HEADING);
+    private static final double RIGHT_HIGH_GOAL_Y = 0;
+    private static final double RIGHT_HIGH_GOAL_SCORE_X = 28;
+
+    private static final Pose2d RIGHT_START_POSE = new Pose2d(RIGHT_CENTER_X, WALL_POS, HEADING);
 
     public RedPath(SampleMecanumDrive drive) {
         this.drive = drive;
     }
 
-    public TrajectorySequence leftPath(Side side) {
-        drive.setPoseEstimate(LEFT_START_POSE);
-
-        TrajectorySequence leftParkSequence = drive.trajectorySequenceBuilder(LEFT_START_POSE)
-                .lineToLinearHeading(LEFT_PARK_MIDDLE)
-                .waitSeconds(1)
-                .lineToLinearHeading(LEFT_PARK_LEFT)
-                .build();
-
-        TrajectorySequence middleParkSequence = drive.trajectorySequenceBuilder(LEFT_START_POSE)
-                .lineToLinearHeading(LEFT_PARK_MIDDLE)
-                .build();
-
-        TrajectorySequence rightParkSequence = drive.trajectorySequenceBuilder(LEFT_START_POSE)
-                .lineToLinearHeading(LEFT_PARK_MIDDLE)
-                .waitSeconds(1)
-                .lineToLinearHeading(LEFT_PARK_RIGHT)
-                .build();
-
-        switch (side) {
-            case ONE:
-                return leftParkSequence;
-            case TWO:
-            case NOT_FOUND:
-                return middleParkSequence;
-            case THREE:
-                return rightParkSequence;
-            default:
-                return middleParkSequence;
-        }
-    }
-
     public TrajectorySequence rightPath(Side side) {
         drive.setPoseEstimate(RIGHT_START_POSE);
 
-        TrajectorySequence leftParkSequence = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
-                .lineToLinearHeading(RIGHT_PARK_MIDDLE)
-                .waitSeconds(1)
-                .lineToLinearHeading(RIGHT_PARK_LEFT)
+        TrajectorySequence sequence = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
+                .lineToLinearHeading(new Pose2d(RIGHT_CENTER_X, RIGHT_HIGH_GOAL_Y, HEADING))
+                .waitSeconds(2)
+                .lineToLinearHeading(new Pose2d(RIGHT_HIGH_GOAL_SCORE_X, RIGHT_HIGH_GOAL_Y, HEADING))
+                .waitSeconds(2)
                 .build();
 
-        TrajectorySequence middleParkSequence = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
-                .lineToLinearHeading(RIGHT_PARK_MIDDLE)
-                .build();
-
-        TrajectorySequence rightParkSequence = drive.trajectorySequenceBuilder(RIGHT_START_POSE)
-                .lineToLinearHeading(RIGHT_PARK_MIDDLE)
-                .waitSeconds(1)
-                .lineToLinearHeading(RIGHT_PARK_RIGHT)
-                .build();
-
-        switch (side) {
-            case ONE:
-                return leftParkSequence;
-            case TWO:
-            case NOT_FOUND:
-                return middleParkSequence;
-            case THREE:
-                return rightParkSequence;
-            default:
-                return middleParkSequence;
-        }
+        return sequence;
     }
 
 }
