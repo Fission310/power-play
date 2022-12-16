020a5a36e72b15b6c5afbd2bf4b9c05e942ecd4d
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-12-16 14:28:55 -0500

    add field-centric option

--- hardware/Drivetrain.java
+++ hardware/Drivetrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -15,6 +16,12 @@ public class Drivetrain extends Mechanism {
 
     public Drivetrain(LinearOpMode opMode) { this.opMode = opMode; }
 
+    public enum DriveMode {
+        ROBOT_CENTRIC,
+        FIELD_CENTRIC
+    }
+    DriveMode driveMode = DriveMode.ROBOT_CENTRIC;
+
     @Override
     public void init(HardwareMap hwMap) {
         rrDrive = new SampleMecanumDrive(hwMap);
@@ -23,13 +30,38 @@ public class Drivetrain extends Mechanism {
 
     @Override
     public void loop(Gamepad gamepad) {
-        rrDrive.setWeightedDrivePower(
-                new Pose2d(
+
+        if (gamepad.left_stick_button) {
+            driveMode = DriveMode.ROBOT_CENTRIC;
+        } else if (gamepad.right_stick_button) {
+            driveMode = DriveMode.FIELD_CENTRIC;
+        }
+
+        switch (driveMode) {
+            case ROBOT_CENTRIC:
+                rrDrive.setWeightedDrivePower(
+                        new Pose2d(
+                                -gamepad.left_stick_y,
+                                -gamepad.left_stick_x,
+                                -gamepad.right_stick_x
+                        )
+                );
+                break;
+            case FIELD_CENTRIC:
+                Vector2d input = new Vector2d(
                         -gamepad.left_stick_y,
-                        -gamepad.left_stick_x,
-                        -gamepad.right_stick_x
-                )
-        );
+                        -gamepad.left_stick_x
+                ).rotated(rrDrive.getPoseEstimate().getHeading());
+
+                rrDrive.setWeightedDrivePower(
+                        new Pose2d(
+                                input.getX(),
+                                input.getY(),
+                                -gamepad.right_stick_x
+                        )
+                );
+                break;
+        }
 
         rrDrive.update();
     }
