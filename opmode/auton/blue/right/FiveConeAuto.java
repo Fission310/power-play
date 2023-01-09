eebeb0e1604feebde7e96ca64a661364d1db276a
Paul Serbanescu <paulserbanescu3@gmail.com>
2023-01-09 11:43:23 -0500

    initial untuned scoring functionality

--- opmode/auton/blue/right/FiveConeAuto.java
+++ opmode/auton/blue/right/FiveConeAuto.java
@@ -1,18 +1,92 @@
 package org.firstinspires.ftc.teamcode.opmode.auton.blue.right;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.opmode.auton.AutoConstants;
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Clamp;
+import org.firstinspires.ftc.teamcode.hardware.SignalSleeveWebcam;
+import org.firstinspires.ftc.teamcode.hardware.SlidesMotors;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import org.firstinspires.ftc.teamcode.opmode.auton.AutoConstants;
 
 @Autonomous (name = "BLUE RIGHT 5 Cone Auto", group = "_ablue")
 public class FiveConeAuto extends LinearOpMode {
 
     private SampleMecanumDrive drive;
+    private Clamp clamp;
+    private Arm arm;
+    private SlidesMotors slides;
+    private SignalSleeveWebcam signalSleeveWebcam = new SignalSleeveWebcam(this, "SignalSleeveWebcam");
+
+    private SignalSleeveWebcam.Side parkSide;
+
+    private Thread scoreReadyThread;
+    private Thread scoreThread;
+
+    private Thread pickupReadyThread;
+    private Thread pickupThread;
+
+    ElapsedTime time = new ElapsedTime();
+
+    public Runnable scoreReady = () -> {
+        try {
+            // extend slides lvl 3
+            // rotate arm to intake pos
+            slides.extendHigh();
+            Thread.sleep(100);
+            arm.intakePos();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    public Runnable score = () -> {
+        try {
+            clamp.open();
+            Thread.sleep(100);
+            arm.scorePos();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    public Runnable pickupReady = () -> {
+        try {
+            // open clamp
+            // rotate arm to score pos (back intake)
+            // retract slides to cone stack pos
+            clamp.open();
+            Thread.sleep(100);
+            arm.scorePos();
+            Thread.sleep(500);
+            slides.extendConeStack();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    public Runnable pickup = () -> {
+        try {
+            clamp.close();
+            Thread.sleep(100);
+            slides.extendHigh();
+            Thread.sleep(100);
+            arm.intakePos();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+
+    public void runThread(Thread thread) {
+        try {
+            thread.start();
+        } catch (IllegalThreadStateException ignored) {}
+    }
 
     private enum TrajectoryState {
         PRELOAD,
@@ -31,6 +105,22 @@ public class FiveConeAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         drive = new SampleMecanumDrive(hardwareMap);
+        clamp = new Clamp(this);
+        arm = new Arm(this);
+        slides = new SlidesMotors(this);
+        signalSleeveWebcam.init(hardwareMap);
+
+        parkSide = signalSleeveWebcam.side();
+
+        clamp.init(hardwareMap);
+        arm.init(hardwareMap);
+        slides.init(hardwareMap);
+
+        scoreReadyThread = new Thread(scoreReady);
+        scoreThread = new Thread(score);
+
+        pickupReadyThread = new Thread(pickupReady);
+        pickupThread = new Thread(pickup);
 
         drive.setPoseEstimate(AutoConstants.BR_START_POSE);
 
@@ -69,7 +159,12 @@ public class FiveConeAuto extends LinearOpMode {
                 .lineToLinearHeading(AutoConstants.BR_PARK_MIDDLE)
                 .build();
 
+        clamp.close();
+        arm.intakePos();
+
         waitForStart();
+
+        signalSleeveWebcam.stopStreaming();
 
         // drive to preload
         drive.followTrajectorySequenceAsync(preload);
@@ -79,44 +174,86 @@ public class FiveConeAuto extends LinearOpMode {
             telemetry.addData("current state", trajectoryState);
             telemetry.update();
             drive.update();
+            slides.update();
 
             switch (trajectoryState) {
                 case PRELOAD:
+                    runThread(scoreReadyThread);
+                    time.reset();
                     if (!drive.isBusy()) {
-                        trajectoryState = TrajectoryState.PRELOAD_TO_CS;
-                        drive.followTrajectorySequenceAsync(preloadToConeStack);
-                        conesScored += 1;
+                        runThread(scoreThread);
+                        if (time.seconds() >= AutoConstants.DELAY_SCORE) {
+                            trajectoryState = TrajectoryState.PRELOAD_TO_CS;
+                            drive.followTrajectorySequenceAsync(preloadToConeStack);
+                            conesScored += 1;
+                        }
                     }
                     break;
                 case PRELOAD_TO_CS:
+                    runThread(pickupReadyThread);
+                    time.reset();
                     if (!drive.isBusy()) {
-                        trajectoryState = TrajectoryState.CS_TO_HG;
-                        drive.followTrajectorySequenceAsync(coneStackToHighGoal);
-                    }
-                    break;
-                case CS_TO_HG:
-                    if (!drive.isBusy()) {
-                        trajectoryState = TrajectoryState.HG_TO_CS;
-                        drive.followTrajectorySequenceAsync(highGoalToConeStack);
-                        conesScored += 1;
-                    }
-                    break;
-                case HG_TO_CS:
-                    if (conesScored == CONE_COUNT) {
-                        trajectoryState = TrajectoryState.PARK;
-                    } else {
-                        if (!drive.isBusy()) {
+                        runThread(pickupThread);
+                        if (time.seconds() >= AutoConstants.DELAY_PICKUP) {
                             trajectoryState = TrajectoryState.CS_TO_HG;
                             drive.followTrajectorySequenceAsync(coneStackToHighGoal);
                         }
                     }
                     break;
-                case PARK:
+                case CS_TO_HG:
+                    runThread(scoreReadyThread);
                     if (!drive.isBusy()) {
-                        trajectoryState = TrajectoryState.IDLE;
-                        drive.followTrajectorySequenceAsync(toLeftPark);
+                        runThread(scoreThread);
+                        if (time.seconds() >= AutoConstants.DELAY_SCORE) {
+                            trajectoryState = TrajectoryState.HG_TO_CS;
+                            drive.followTrajectorySequenceAsync(highGoalToConeStack);
+                            conesScored += 1;
+                        }
                     }
-                    // switch webcam
+                    break;
+                case HG_TO_CS:
+                    if (conesScored == CONE_COUNT) {
+                        slides.rest();
+                        arm.intakePos();
+                        clamp.open();
+                        trajectoryState = TrajectoryState.PARK;
+                    } else {
+                        runThread(pickupReadyThread);
+                        if (!drive.isBusy()) {
+                            runThread(pickupThread);
+                            if (time.seconds() >= AutoConstants.DELAY_PICKUP) {
+                                trajectoryState = TrajectoryState.CS_TO_HG;
+                                drive.followTrajectorySequenceAsync(coneStackToHighGoal);
+                            }
+                        }
+                    }
+                    break;
+                case PARK:
+                    switch (parkSide) {
+                        case ONE:
+                            if (!drive.isBusy()) {
+                                trajectoryState = TrajectoryState.IDLE;
+                            }
+                            break;
+                        case TWO:
+                            if (!drive.isBusy()) {
+                                trajectoryState = TrajectoryState.IDLE;
+                                drive.followTrajectorySequenceAsync(toMiddlePark);
+                            }
+                            break;
+                        case THREE:
+                            if (!drive.isBusy()) {
+                                trajectoryState = TrajectoryState.IDLE;
+                                drive.followTrajectorySequenceAsync(toLeftPark);
+                            }
+                            break;
+                        case NOT_FOUND:
+                        default:
+                            if (!drive.isBusy()) {
+                                trajectoryState = TrajectoryState.IDLE;
+                            }
+                            break;
+                    }
                     break;
                 case IDLE:
                     break;
