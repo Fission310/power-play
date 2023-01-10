e685052a5e717bfcae1ef6bdb43a9c6b47c4bb92
Paul Serbanescu <paulserbanescu3@gmail.com>
2023-01-10 13:49:49 -0500

    opmode to fix extended slides after auto

---
+++ opmode/teleop/FixSlides.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Arm;
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+import org.firstinspires.ftc.teamcode.hardware.SlidesMotors;
+
+@TeleOp (name = "FIX Slides", group = "_zmain")
+public class FixSlides extends LinearOpMode {
+
+    private Drivetrain drivetrain = new Drivetrain(this);
+    private SlidesMotors slidesMotors = new SlidesMotors(this);
+    private Arm arm = new Arm(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivetrain.init(hardwareMap);
+        slidesMotors.init(hardwareMap);
+        arm.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            drivetrain.loop(gamepad1);
+
+            if (gamepad1.right_trigger > 0) {
+                slidesMotors.setPower(0.7);
+            } else if (gamepad1.left_trigger > 0) {
+                slidesMotors.setPower(-0.7);
+            } else if (gamepad1.dpad_up) {
+                arm.scorePos();
+            } else if (gamepad1.dpad_down) {
+                arm.intakePos();
+            }
+        }
+    }
+
+}
